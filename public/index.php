b3b6665bff322f155ec7a7c242d0cf1d0e21b027
Pedro Henrique Alexandre <121180098+phalexandre10@users.noreply.github.com>
2025-05-16 19:41:22 -0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teste PHP no Codespace</title>
+</head>
+<body>
+    <h1>Olá do PHP no Codespace!</h1>
+    <p>A versão do PHP é: <?php echo phpversion(); ?></p>
+    <p>A data atual é: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
+
